784e07d8acf14d370f6de8cbf1130fe750ce0652
Denis Valdivieso <sshshaka@gmail.com>
2021-11-01 08:22:22 -0400

    fetch a la API

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+const Button = styled.button`
+  background: -webkit-linear-gradient(
+    top left,
+    #007d35 0%,
+    #007d35 40%,
+    #0f574e 100%
+  );
+
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size 0.8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
+
 function App() {
-  return <h1>Breaking Bad</h1>;
+  const handleClick = async () => {
+    const api = await fetch("https://www.breakingbadapi.com/api/quote/random");
+    const quote = await api.json();
+    console.log(quote[0]);
+  };
+
+  return (
+    <Container>
+      <Button onClick={handleClick}>Obtener frase</Button>
+    </Container>
+  );
 }
 
 export default App;

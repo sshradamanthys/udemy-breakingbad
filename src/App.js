8ad5ccc76767603f4fe7981d22bffb4365d0573d
Denis Valdivieso <sshshaka@gmail.com>
2021-11-01 13:37:56 -0400

    Styles

--- src/App.js
+++ src/App.js
@@ -1,51 +1,52 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Display from "./components/Display";
+import logo from "./logo.svg";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
   display: flex;
-  align-items: center;
-  padding-top: 5rem;
   flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
 `;
 
 const Button = styled.button`
-  background: -webkit-linear-gradient(
-    top left,
-    #007d35 0%,
-    #007d35 40%,
-    #0f574e 100%
-  );
-
-  background-size: 300px;
-  font-family: Arial, Helvetica, sans-serif;
-  color: #fff;
-  margin-top: 3rem;
-  padding: 1rem 3rem;
-  font-size: 2rem;
-  border: 2px solid black;
-  transition: background-size 0.8s ease;
+  background: linear-gradient(to top left, #007d35, #0f574e, #00ff00);
+  border: 2px solid rgba(0, 0, 0, 0.25);
+  color: whitesmoke;
+  font-size: 1rem;
+  padding: 0.5rem 1.5rem;
+  margin-top: 1rem;
 
   :hover {
     cursor: pointer;
-    background-size: 400px;
+    background: linear-gradient(to bottom right, #007d35, #0f574e, #000);
   }
+`;
+
+const Img = styled.img`
+  width: 25%;
+  opacity: 0.7;
 `;
 
 function App() {
   const [quote, setQuote] = useState({});
 
-  const handleClick = async () => {
+  const fetchAPI = async () => {
     const res = await fetch("https://www.breakingbadapi.com/api/quote/random");
     const arr = await res.json();
-    const first = arr[0];
-    setQuote(first);
+    const obj = arr[0];
+    setQuote(obj);
   };
+
+  useEffect(() => fetchAPI(), []);
 
   return (
     <Container>
+      <Img src={logo} alt="logo" />
       <Display quote={quote} />
-      <Button onClick={handleClick}>Obtener frase</Button>
+      <Button onClick={fetchAPI}>Quote</Button>
     </Container>
   );
 }
--- src/components/Display.js
+++ src/components/Display.js
@@ -1,9 +1,40 @@
+import styled from "@emotion/styled";
+
+const Div = styled.div`
+  background-color: whitesmoke;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+  margin-top: 1rem;
+
+  h1 {
+    margin-top: 0;
+    margin-left: 1rem;
+    position: relative;
+    &::before {
+      content: open-quote;
+      font-size: 3rem;
+      color: black;
+      position: absolute;
+      left: -2rem;
+      top: -1rem;
+    }
+  }
+
+  p {
+    color: rgba(0, 0, 0, 0.8);
+    text-align: right;
+  }
+`;
+
 const Display = ({ quote }) => {
   return (
-    <div>
-      <p>Quote: {quote.quote}</p>
-      <p>Author: {quote.author}</p>
-    </div>
+    <Div>
+      <h1>{quote.quote} </h1>
+      <p>- {quote.author} </p>
+    </Div>
   );
 };
 
